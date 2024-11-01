f7900d1b2b5263bbf9336553b5da0ac731ace3db
brunocssouza <92397493+brunocssouza@users.noreply.github.com>
2024-11-01 08:26:44 -0300

    Update exercicios-interpretacao.js

--- 28-10/exercicios-interpretacao.js
+++ 28-10/exercicios-interpretacao.js
@@ -1,18 +1,24 @@
 // Interpretação
+// 1.
 let array
 console.log('a. ', array) // Output: a. undefined
 
+// 2.
 array = null
 console.log('b. ', array) // Output: b. null
 
+// 3.
 array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 console.log('c. ', array.length) // Output: a. 11
 
+// 4.
 let i = 0
 console.log('d. ', array[i]) // Output: d. 3
 
+// 5.
 array[i+1] = 19
 console.log('e. ', array) // Output: e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+// 6.
 const valor = array[i+6]
 console.log('f. ', valor) // Output: f. 8
