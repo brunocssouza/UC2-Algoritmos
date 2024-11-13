5c304e0da8217b3048a7d9185b6353ee73f01ba8
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2024-11-13 16:58:38 -0300

    Delete 12-11 directory

--- 12-11/Exercicios/Escrita/Escrita.js
+++
@@ -1,8 +0,0 @@
-// a)
-const pessoa = {
-    nome: "Vitor Hugo", 
-    apelidos: ["Vitinho", "Vitão", "Vit"]
- }
- 
-
- console.log(`Eu sou ${pessoa.nome}, mas pode me chamr de: ${pessoa.apelidos[0]}, ${pessoa.apelidos[1]} ou ${pessoa.apelidos[2]}.`)
