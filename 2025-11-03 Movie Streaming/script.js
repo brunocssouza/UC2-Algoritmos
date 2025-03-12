6634b732948e78392c27d0ad49a748897dde6f8c
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-03-11 22:28:07 -0300

    Add files via upload

---
+++ 2025-11-03 Movie Streaming/script.js
@@ -0,0 +1,11 @@
+const filmes = {
+    acao: [
+        {
+            nome: "John Wick",
+            diretor: "Chad Stahelski",
+            elenco: ["Keanu Reeves", "Michael Nyqvist", "Alfie Allen"],
+            sinopse: "A legendary hitman who comes out of retirement to seek revenge against the men who killed his dog.",
+            imagem: ""
+        }
+    ]
+}
