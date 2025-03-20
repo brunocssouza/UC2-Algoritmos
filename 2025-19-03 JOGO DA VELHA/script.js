5317263fde8c19c61bb12aefa2e7c77919cdc1d0
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-03-19 21:59:57 -0300

    Add files via upload

---
+++ 2025-19-03 JOGO DA VELHA/script.js
@@ -0,0 +1,10 @@
+let tabuleiro = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+];
+
+let vez = "L";          // Começa com o jogador "L" (Faz o L).
+let jogoAtivo = true;   // Enquanto o jogo nao estiver finalizado, será 'true'.
+
+// Função para renderizar o tabuleiro.
