0ae649b97bcdba0f266577ab987cd43849c0523a
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-03-20 22:03:54 -0300

    Add files via upload

---
+++ 2025-20-03 LOOPS/enum.ts
@@ -0,0 +1,20 @@
+// ENUM: nomeia um conjunto de CONSTANTES.
+enum dias_semana {
+    seguinda,
+    terca,
+    quarta,
+    quinta,
+    sexta
+}
+
+console.log(dias_semana.quinta);
+
+//
+export enum Cores {
+
+}
+
+//
+export class SUrivor {
+    private nome:string;
+}
